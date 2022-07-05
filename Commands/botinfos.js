246a9ec7bba1d76ce51f4a6042bd44810569e71f
Satanic <104293834+MehdiSatanic@users.noreply.github.com>
2022-07-05 12:32:17 +0430

    Add files via upload

---
+++ Commands/botinfos.js
@@ -0,0 +1,19 @@
+const {
+  SlashCommandBuilder
+} = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('botinfo')
+    .setDescription('Gives the credits of the bot.'),
+  async execute(interaction, client) {
+    const embed = new client.discord.MessageEmbed()
+      .setColor('303236')
+      .setDescription('Ticket-Bot)
+      .setFooter(client.user.tag, client.user.avatarURL())
+      .setTimestamp();
+    await interaction.reply({
+      embeds: [embed]
+    });
+  },
+};
